bc194c65237c6a10fedbfd08b6cbcf818b2ea5f0
Danijar Hafner <mail@danijar.com>
2015-11-11 16:54:53 +0100

    Add setup script and coverage reports

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='layered',
+    version='0.1.0',
+    description='Clean reference implementation of feed forward neural networks',
+    url='http://github.com/danijar/layered',
+    author='Danijar Hafner',
+    author_email='mail@danijar.com',
+    license='MIT',
+    packages=['layered'])
