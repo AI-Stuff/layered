92421fd47f74247e36d542a19a7a50061c96c04c
Danijar Hafner <mail@danijar.com>
2015-11-12 15:08:25 +0100

    Refactor example class to use properties

--- layered/example.py
+++ layered/example.py
@@ -2,24 +2,26 @@ class Example:
     """
     Immutable class representing one example in a dataset.
     """
-    __slots__ = ('data', 'target')
+    __slots__ = ('_data', '_target')
 
     def __init__(self, data, target):
-        super().__setattr__('data', data)
-        super().__setattr__('target', target)
+        self._data = data
+        self._target = target
 
-    def __setattr__(self, *args):
-        raise TypeError
+    @property
+    def data(self):
+        return self._data
 
-    def __delattr__(self, *args):
-        raise TypeError
+    @property
+    def target(self):
+        return self._target
 
     def __getstate__(self):
         return {'data': self.data, 'target': self.target}
 
     def __setstate__(self, state):
-        super().__setattr__('data', state['data'])
-        super().__setattr__('target', state['target'])
+        self._data = state['data']
+        self._target = state['target']
 
     def __repr__(self):
         data = ' '.join(str(round(x, 2)) for x in self.data)
